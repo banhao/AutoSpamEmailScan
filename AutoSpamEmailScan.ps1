9d30c5e7fe77a76aacf8993c57dda0d7fb9d3688
banhao <banhao@gmail.com>
2019-08-20 16:51:19 -0600

    add powershellgallery property

--- AutoSpamEmailScan.ps1
+++ AutoSpamEmailScan.ps1
@@ -1,3 +1,51 @@
+
+<#PSScriptInfo
+
+.VERSION 3.3
+
+.GUID 134de175-8fd8-4938-9812-053ba39eed83
+
+.AUTHOR banhao@gmail.com
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ AutoSpamEmailScan.ps1 is used to monitor a specific mailbox that in enterprise users can forward suspicious spam emails to a specific mailbox. 
+This PowerShell script can monitor the mailbox for any unread emails, grab the URLs and attachments from the emails and submit to virustotal.com, urlscan.io, Google safe browsing and OPSWAT. Script also can extract URLs from a pdf file. 
+After the scan finished, script can generate HTML format scan report and auto reply to the senders.
+Script can be run once or loop interval, if  in the init.conf is 0 means script will only run one time else the number is the loop interval seconds.
+
+Before you run the script Install the Exchange Web Services Managed API 2.2. 
+https://www.microsoft.com/en-us/download/details.aspx?id=42951 
+
+#> 
+
+Param()
+
+
 <#
 .SYNOPSIS
   <>
