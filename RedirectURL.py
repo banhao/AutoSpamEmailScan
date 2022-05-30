7e731e38cb3d25de419b44c83c3b24af928c8616
banhao <banhao@gmail.com>
2022-05-30 15:09:12 -0600

    Add files via upload

--- RedirectURL.py
+++ RedirectURL.py
@@ -6,13 +6,21 @@
 # Issue Date: May 25, 2022
 # Release Note: 
 
-import sys, time, datetime
+import sys, time, datetime, urllib.request
 from selenium import webdriver
 
 url = sys.argv[1]
-driver = webdriver.Edge(executable_path=r'.\msedgedriver.exe')
-time.sleep(2)
-driver.get(url)
-time.sleep(5)
-print(driver.current_url)
-driver.quit()
+try:
+    response_code = urllib.request.urlopen(url).getcode()
+except Exception:
+    sys.exc_info()
+    print(url, "is not accessible.")
+    response_code = 404
+
+if response_code == 200:
+    driver = webdriver.Edge(executable_path=r'.\msedgedriver.exe')
+    time.sleep(2)
+    driver.get(url)
+    time.sleep(2)
+    print(driver.current_url)
+    driver.quit()
